fb7b6cce6d1755eec433092c99507dab81b6e243
Elly Maimon and Scott Fraser <elly@>
2018-05-29 15:52:50 -0700

    added 2 more rows

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(function() {
     $("#h3-javascript").hide();
   });
 
+//First Row
   $("#p-javascript").click(function(){
     $("#p-javascript").fadeOut();
     $("#h3-javascript").fadeIn();
@@ -39,6 +40,87 @@ $(function() {
     $("#p-vnm").fadeOut();
     $("#h3-vnm").fadeIn();
   });
+//Second Row
 
+  $("#h3-methods").click(function(){
+    $("#p-methods").fadeIn();
+    $("#h3-methods").hide();
+  });
+
+  $("#p-methods").click(function(){
+    $("#p-methods").fadeOut();
+    $("#h3-methods").fadeIn();
+  });
+
+  $("#h3-arguments").click(function(){
+    $("#p-arguments").fadeIn();
+    $("#h3-arguments").hide();
+  });
+
+  $("#p-arguments").click(function(){
+    $("#p-arguments").fadeOut();
+    $("#h3-arguments").fadeIn();
+  });
+
+  $("#h3-parameters").click(function(){
+    $("#p-parameters").fadeIn();
+    $("#h3-parameters").hide();
+  });
+
+  $("#p-parameters").click(function(){
+    $("#p-parameters").fadeOut();
+    $("#h3-parameters").fadeIn();
+  });
+
+  $("#h3-functions").click(function(){
+    $("#p-functions").fadeIn();
+    $("#h3-functions").hide();
+  });
+
+  $("#p-functions").click(function(){
+    $("#p-functions").fadeOut();
+    $("#h3-functions").fadeIn();
+  });
+//third row
+
+$("#h3-return").click(function(){
+  $("#p-return").fadeIn();
+  $("#h3-return").hide();
+});
+
+$("#p-return").click(function(){
+  $("#p-return").fadeOut();
+  $("#h3-return").fadeIn();
+});
+
+$("#h3-chaining").click(function(){
+  $("#p-chaining").fadeIn();
+  $("#h3-chaining").hide();
+});
+
+$("#p-chaining").click(function(){
+  $("#p-chaining").fadeOut();
+  $("#h3-chaining").fadeIn();
+});
+
+$("#h3-strings").click(function(){
+  $("#p-strings").fadeIn();
+  $("#h3-strings").hide();
+});
+
+$("#p-strings").click(function(){
+  $("#p-strings").fadeOut();
+  $("#h3-strings").fadeIn();
+});
+
+$("#h3-booleans").click(function(){
+  $("#p-booleans").fadeIn();
+  $("#h3-booleans").hide();
+});
+
+$("#p-booleans").click(function(){
+  $("#p-booleans").fadeOut();
+  $("#h3-booleans").fadeIn();
+});
 
 });
